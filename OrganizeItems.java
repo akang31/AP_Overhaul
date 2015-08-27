ee8fae0668a45ce65ee5a71b9a8f604121cd98a4
dwkang1111 <dwkang1111@gmail.com>
2015-08-27 02:00:22 -0400

    search function fully capable

--- OrganizeItems.java
+++ OrganizeItems.java
@@ -1,9 +1,9 @@
 import java.io.*;
 import java.util.*;
 
-public class OrganizeChamps {
+public class OrganizeItems {
     public static void main(String[] args) throws IOException{
-        BufferedReader reader = new BufferedReader(new FileReader("listofchamps.txt"));
+        BufferedReader reader = new BufferedReader(new FileReader("listofitems.txt"));
         String current = "";
         String totalName = "";
         String totalVal = "";
@@ -11,15 +11,9 @@ public class OrganizeChamps {
         int[] values = new int[289];
         int count = 0;
         while ((current = reader.readLine()) != null) {
-            String currentName = "";
+            totalVal += current.substring(current.length() - 4, current.length()) + ",";
+            current = current.substring(0, current.length() - 4);
             int max = 0;
-            for(int i = current.length() - 1; i >= 0; i--)
-                if(current.charAt(i) == ' ')
-                    break;
-                else
-                    currentName = current.charAt(i) + currentName;
-            current = current.substring(0, current.length() - currentName.length());
-            totalVal += currentName + ",";
             for(int i = current.length() - 1; i >= 0; i--)
                 if(current.charAt(i) == ' ')
                     max = i;
@@ -34,7 +28,7 @@ public class OrganizeChamps {
             totalName += "\'" + current + "\',";
             count++;
         }
-        PrintWriter writer = new PrintWriter("listofchamps.min", "UTF-8");
+        PrintWriter writer = new PrintWriter("listofitems.min", "UTF-8");
         writer.println(totalName);
         writer.println(totalVal);
         writer.close();
